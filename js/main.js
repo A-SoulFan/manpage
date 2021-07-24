564400765592e630742b416f8c1dcaf18cf0a408
dongwenhao <dwhandwp@163.com>
2021-07-24 15:40:14 +0800

    part3 animation

--- js/main.js
+++ js/main.js
@@ -164,6 +164,9 @@ $(document).ready(function($) {
         });
     };
     OnePageNav();
+
+    // part1 animation
+    part1AnimationClass()
 });
 //getosinfo
 function getOsInfo() {
@@ -216,3 +219,37 @@ $(".navbar-toggler").blur(function(event) {
         $(".navbar-collapse").collapse('hide');
     }
 });
+
+
+function part1AnimationClass() {
+
+    var part1Top = $("#GroupIntro").offset().top
+    var windowHeight = $(window).height()
+    var part1Height = $("#GroupIntro").outerHeight()
+    var screenWidth = window.innerWidth;
+
+    part1Animation()
+    $(window).scroll(function(e) {
+        part1Animation()
+    })
+    function part1Animation() {
+        var scrollTop = $(document).scrollTop();
+        var docTop = Number(scrollTop) + Number(windowHeight)
+        if (docTop > part1Top && scrollTop < part1Top + part1Height) {
+            $(".patr1-img img").css("transform", "translateY(0)")
+            if (screenWidth < 768) {
+                $(".part1-mobile-introduce").css("transform", "translateY(0)")
+            } else {
+                $(".part-introduce").css("transform", "translateY(0)")
+            }
+        } else {
+            if (screenWidth < 768) {
+                $(".patr1-img img").css("transform", "translateY(150px)")
+                $(".part1-mobile-introduce").css("transform", "translateY(-150px)")
+            } else {
+                $(".patr1-img img").css("transform", "translateY(300px)")
+                $(".part-introduce").css("transform", "translateY(-300px)")
+            }
+        }
+    }
+}
